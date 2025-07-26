a3c1ba6e7c77fa8a5329ee4ba76a07c1acd4b14f
Kono <anupam.nitrkl@gmail.com>
2025-07-26 09:12:35 +0530

    feat: implement initial views and routing

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const session = require("express-session");
 
 const app = express();
 
-let indexRouter = require("./routes/index");
-let authRouter = require("./routes/auth");
-app.locals.pluralize = require("pluralize");
+
 
 console.log(" Starting Anupam's Express application...");
 
@@ -23,6 +21,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
 app.listen(3000, () => {
   console.log("Server Started on Port 3000");
